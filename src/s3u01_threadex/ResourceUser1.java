04c53078cba09759966c81ef034797195ceb9ad5
Weiqifan1 <cmlykke@hotmail.com>
2018-02-04 14:42:13 +0100

    dag 2 opgave 2: tilfoejet kommentarer 3

--- src/s3u01_threadex/ResourceUser1.java
+++ src/s3u01_threadex/ResourceUser1.java
@@ -6,14 +6,81 @@ import java.util.logging.Logger;
 
 class ResourceUser1 extends Thread {
 
-  ResourceContainer resource;
+    ResourceContainer resource;
 
-  ResourceUser1(ResourceContainer r) {
-    resource = r;
+    ResourceUser1(ResourceContainer r) {
+        resource = r;
+    }
+    boolean doRun = true;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+
+            try {
+                System.out.println(Thread.currentThread().getName() + " Get resource WORDS");
+                List<String> words = resource.getResourceWords();
+                words.add("Peter");
+                words.add("Kurt");
+                words.add("Hanne");
+                Thread.sleep(1);//Simulate that using the resource takes som time 
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ResourceUser1.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                resource.releaseResourceWords();
+            }
+
+            System.out.println(Thread.currentThread().getName() + " Get resource NUMBERS");
+            List<Integer> numbers = resource.getResourceNumbers();
+            numbers.add(1);
+            numbers.add(2);
+            numbers.add(3);
+
+            System.out.println(Thread.currentThread().getName() + " Done with resources");
+
+            resource.releaseResourceNumbers();
+
+        }
+    }
+}
+
+/*
+//original kode:
+
+@Override
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      try {
+        System.out.println(Thread.currentThread().getName()+ " Get resource WORDS");
+        List<String> words = resource.getResourceWords();
+        words.add("Peter");
+        words.add("Kurt");
+        words.add("Hanne");
+        Thread.sleep(1);//Simulate that using the resource takes som time 
+        System.out.println(Thread.currentThread().getName()+ " Get resource NUMBERS");
+        List<Integer> numbers = resource.getResourceNumbers();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        
+        System.out.println(Thread.currentThread().getName() + " Done with resources");
+      } catch (InterruptedException ex) {
+        Logger.getLogger(ResourceUser1.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      finally{
+        resource.releaseResourceNumbers();
+        resource.releaseResourceWords();
+      }
+    }
   }
-  boolean doRun = true;
+ */
 
-  @Override
+
+
+/*
+// mulig deadlock loesning:
+
+@Override
   public void run() {
     for (int i = 0; i < 100; i++) {
         
@@ -45,37 +112,6 @@ class ResourceUser1 extends Thread {
       //finally{
         resource.releaseResourceNumbers();
       //}
-    }
-  }
-}
-
-/*
-//original kode:
-
-@Override
-  public void run() {
-    for (int i = 0; i < 100; i++) {
-      try {
-        System.out.println(Thread.currentThread().getName()+ " Get resource WORDS");
-        List<String> words = resource.getResourceWords();
-        words.add("Peter");
-        words.add("Kurt");
-        words.add("Hanne");
-        Thread.sleep(1);//Simulate that using the resource takes som time 
-        System.out.println(Thread.currentThread().getName()+ " Get resource NUMBERS");
-        List<Integer> numbers = resource.getResourceNumbers();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        
-        System.out.println(Thread.currentThread().getName() + " Done with resources");
-      } catch (InterruptedException ex) {
-        Logger.getLogger(ResourceUser1.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      finally{
-        resource.releaseResourceNumbers();
-        resource.releaseResourceWords();
-      }
     }
   }
 */
--- src/s3u01_threadex/TesterDag2Opg5.java
+++ src/s3u01_threadex/TesterDag2Opg5.java
@@ -1,19 +1,27 @@
 package s3u01_threadex;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class Tester {
-  public static void main(String[] args) {
+public class TesterDag2Opg5 {
+  public static void main(String[] args) { 
+    
     
     try {
       ResourceContainer resources = new ResourceContainer();
       ResourceUser1 t1 = new ResourceUser1(resources);
       ResourceUser2 t2 = new ResourceUser2(resources);
+      DeadLockDetector dd = new DeadLockDetector(new Thread[]{t1,t2});
+      
       t1.start();
       t2.start();
-          
+      
+      dd.run();  
+      
       t1.join();
       t2.join();
       
@@ -21,7 +29,7 @@ public class Tester {
       System.out.println("Words produced: "+resources.getResourceWords().size());
       System.out.println("Numbers produced: "+resources.getResourceNumbers().size());
     } catch (InterruptedException ex) {
-      Logger.getLogger(Tester.class.getName()).log(Level.SEVERE, null, ex);
+      Logger.getLogger(TesterDag2Opg5.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
 }
